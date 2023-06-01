8d934e8531a9dfb5d2381c2a67f5628b2c064ba6
Sam <129320175+samsauder@users.noreply.github.com>
2023-06-01 14:48:23 -0700

    Add files via upload

--- TextCleaner.java
+++ TextCleaner.java
@@ -2,7 +2,11 @@
  * This class cleans up text with a combination of lowercasing,
  * non-alpha removal, stop word removal and stemming.
  */
+import java.io.*;
+import java.util.*;
+
 public class TextCleaner {
+  ArrayList<String> stopwords;
     /**
      * Create a TextCleaner object
      */
@@ -16,7 +20,41 @@ public class TextCleaner {
      * @return The cleaned word
      */
     public String clean(String word) {
-        return null;
+      //Set word to lowercase
+        word = word.toLowerCase();
+
+      //Check if word is  a stop word
+      readStopWords();
+      if(stopwords.contains(word)) {
+        return "";
+      }
+        //Remove all non-alphabetic characters
+        for(int i = 0; i < word.length(); i++) {
+          char c = word.charAt(i);
+          if(!Character.isAlphabetic(c)) {
+            word = word.replace(Character.toString(c), "");
+          }
+        }
+
+        //Get root word of the word
+        PorterStemmer stemmer = new PorterStemmer();
+        String result = stemmer.stem(word);
+        return result;
+    }
+
+    public void readStopWords() {
+      stopwords = new ArrayList<String>();
+      try {
+        FileInputStream fis = new FileInputStream("Lib/stopwords.txt");
+        Scanner s = new Scanner(fis);
+        while(s.hasNextLine()) {
+          String word = s.nextLine();
+          stopwords.add(word);
+        }
+      }
+      catch(Exception e) {
+
+      }
     }
 
     /**
@@ -26,6 +64,9 @@ public class TextCleaner {
      * @return A new array of words which are all cleaned.
      */
     public String[] clean(String[] words) {
-        return null;
+        for(String s : words) {
+          s = clean(s);
+        }
+        return words;
     }
 }
